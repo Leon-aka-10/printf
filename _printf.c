a6110b7292b191538bc04770ccf99e0037f51577
Leon-aka-10 <chuboy_10@yahoo.com>
2022-05-20 06:42:18 -0700

    updated Task 0 and Task 1

--- _printf.c
+++ _printf.c
@@ -9,11 +9,11 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-
 	int i;
 	int n_displayed = 0;
 
 	va_start(args, format);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
