88f09e008ee5a71a62e34e15dae3677a1ff66eee
JoyOparah <oparahjoy@gmail.com>
2022-05-20 14:39:02 -0700

    updated special function.c

--- _special_func.c
+++ _special_func.c
@@ -32,4 +32,23 @@ void write_base(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		_write(str[i]);
+/* base_len */
+unsigned int base_len(unsigned int num, int base)
+{
+    unsigned int i;
+    for (i = 0; num &gt; 0; i++)
+    {
+        num /= base;
+    }
+    return (i);
+}
+/* _memcpy */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+    for (i = 0; i &lt; n; i++)
+        dest[i] = src[i];
+    dest[i] = &#39;\0&#39;;
+    return (dest);
+}
 }
